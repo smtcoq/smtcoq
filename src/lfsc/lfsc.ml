c772e99e8cb87100a02e8801f4cfa01c714710d1
ekiciburak <ekcburak@hotmail.com>
2016-03-30 09:42:04 -0500

    lfsc parser and lexer

--- src/lfsc/lfsc.ml
+++ src/lfsc/lfsc.ml
@@ -33,6 +33,32 @@ open SmtAtom
 
 (* TODO: replace these dummy functions with complete ones *)
 
+  (** Type of S-expressions *)
+type t = Atom of string | List of t list
+
+
+let rec print fmt = function
+  | Atom s -> Format.pp_print_string fmt s
+  | List l ->
+    Format.fprintf fmt "(";
+    List.iter (Format.fprintf fmt "%a " print) l;
+    Format.fprintf fmt ")"
+
+let rec print_list fmt = function
+  | [] -> ()
+  | s :: r ->
+    Format.fprintf fmt "%a@." print s;
+    print_list fmt r
+
+let rec size = function
+  | Atom _ -> 1
+  | List l -> List.fold_left (fun acc s -> size s + acc) 0 l
+
+let rec size_list = function
+  | [] -> 0
+  | s :: r -> size s + size_list r
+
+
 let import_trace filename =
   let chan = open_in filename in
   let lexbuf = Lexing.from_channel chan in
@@ -54,6 +80,8 @@ let import_all fsmt fproof =
   let roots = Smtlib2_genConstr.import_smtlib2 rt ro ra rf fsmt in
   let () = import_trace fproof in
   ()
+  
+
 
 
 (* let parse_certif t_i t_func t_atom t_form root used_root trace fsmt fproof = *)
