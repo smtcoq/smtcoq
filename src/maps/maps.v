83cda263ab44dac5c7a687074c64486c4f05fc96
ekiciburak <ekcburak@hotmail.com>
2016-05-20 13:45:04 -0500

    using Finite map library

--- src/maps/maps.v
+++ src/maps/maps.v
@@ -1,109 +1,96 @@
-Require Import FMaps.
+Require Export OrderedType.
+Require Export OrderedTypeEx.
+Require Export OrderedTypeAlt.
+Require Export DecidableType.
+Require Export DecidableTypeEx.
+Require Export FSetInterface.
+Require Export FSetBridge.
+Require Export FSetFacts.
+Require Export FSetDecide.
+Require Export FSetProperties.
+Require Export FSetEqProperties.
+Require Export FSetWeakList.
+Require Export FSetList.
+Require Export FSetPositive.
+Require Export FSetAVL.
+Require Import FSetInterface.
 Require Import FMapInterface.
-Require Import FMapList.
+Require Import Coq.FSets.FMapList.
 
-Module Make (X: OrderedType) <: S with Module E := X.
+Set Implicit Arguments.
+Unset Strict Implicit.
 
-SearchAbout Raw.is_empty.
-
-Check Elt.mapi.
-
+(* using Finite map library *)
 
 Module Raw (X:OrderedType).
 
-Module Import MX := OrderedTypeFacts X.
-Module Import PX := KeyOrderedType X.
+Module E := X.
+Module MX := OrderedTypeFacts X.
+Module PX := KeyOrderedType X.
+Import MX.
+Import PX.
 
 Definition key := X.t.
-Definition t (elt:Type) := list (X.t * elt).
+Definition t (elt:Set) := list (X.t * elt).
 
 Section Elt.
-Variable elt : Type.
+Variable elt : Set.
 
-Notation eqk := (eqk (elt:=elt)).
-Notation eqke := (eqke (elt:=elt)).
-Notation ltk := (ltk (elt:=elt)).
-Notation MapsTo := (MapsTo (elt:=elt)).
-Notation In := (In (elt:=elt)).
 Notation Sort := (sort ltk).
 Notation Inf := (lelistA (ltk)).
 
+Function find (k:key) (s: t elt) {struct s} : option elt :=
+ match s with
+  | nil => None
+  | (k',x)::s' =>
+     match X.compare k k' with
+      | LT _ => None
+      | EQ _ => Some x
+      | GT _ => find k s'
+     end
+ end.
 
-
-
-
-
-Require Import Arith Bool.
-Require Import FunctionalExtensionality.
-
-(* total maps in software foundations *)
-
-(* type for the keys that will be used to index into the maps. *)
-Inductive id : Type :=
-  | Id : nat -> id.
-
-Definition beq_id id1 id2 :=
-  match id1, id2 with
-    | Id n1, Id n2 => beq_nat n1 n2
-  end.
-
-(* a total map is a function that look-ups ids and return As. *)
-Definition total_map (A:Type) := id -> A.
-
-Definition empty_map {A:Type} (v : A) : total_map A := (fun _ => v).
-
-(* update on maps:
-takes 
-    a map m, 
-    a key x, 
-    a value v 
-returns 
-    a new map that maps
-    x to v 
-    every other key to whatever m does. *)
-Definition update_map {A:Type} (m : total_map A) (x : id) (v : A) :=
-                      fun x' => if beq_id x x' then v else m x'.
-
-Theorem beq_id_refl : forall id, true = beq_id id id.
-Proof. intros id. destruct id. simpl. now rewrite (beq_nat_refl n). Qed.
-
-Theorem beq_id_diff : forall id1 id2, id1 <> id2 -> false = beq_id id1 id2.
-Proof. intros id1 id2 H.
-       destruct id1, id2 in *.
-       simpl in *. symmetry. apply beq_nat_false_iff. unfold not in *.
-       intro H1. apply H. now rewrite H1.    
- Qed.
-
-Theorem beq_id_true_iff : forall id1 id2 : id, beq_id id1 id2 = true <-> id1 = id2.
-Proof. intros id1 id2. split; intro H. 
-       - destruct id1, id2. unfold beq_id in *.
-         apply beq_nat_true in H. now rewrite H.
-       - rewrite H. now rewrite (beq_id_refl id2).
+Lemma find_2 : forall m x e, find x m = Some e -> MapsTo x e m.
+Proof.
+   intros m x. unfold PX.MapsTo.
+   functional induction (find x m).
+   - intros e H0. inversion H0.
+   - intros e H0. inversion H0.
+   - intros e H0. inversion H0. auto.
+   - intros e H0. inversion H0. auto.
 Qed.
 
-Theorem update_eq : forall A (m: total_map A) x v, (update_map m x v) x = v.
-Proof. intros A m x v. unfold update_map.
-       now rewrite <- beq_id_refl.
+Function add (k : key) (x : elt) (s : t elt) {struct s} : t elt :=
+ match s with
+  | nil => (k,x) :: nil
+  | (k',y) :: l =>
+     match X.compare k k' with
+      | LT _ => (k,x)::s
+      | EQ _ => (k,x)::l
+      | GT _ => (k',y) :: add k x l
+     end
+ end.
+
+Lemma add_1 : forall m x y e, X.eq x y -> MapsTo y e (add x e m).
+Proof.
+   intros m x y e.
+   generalize y; clear y.
+   unfold PX.MapsTo.
+   functional induction (add x e m); simpl; auto.
 Qed.
 
-Theorem update_neq : forall (A: Type) v x1 x2 (m : total_map A), x1 <> x2 -> (update_map m x1 v) x2 = m x2.
-Proof. intros A v x1 x2 m H.
-       unfold update_map.
-       rewrite <- beq_id_diff; trivial.
+Lemma add_2 : forall m x y e e', ~ X.eq x y -> MapsTo y e m -> MapsTo y e (add x e' m).
+Proof.
+   intros m x y e e'.
+   generalize y e; clear y e; unfold PX.MapsTo.
+   functional induction (add x e' m) ;simpl;auto; clear e0.
+   - intros y0 e H1; inversion_clear 1; destruct H0; simpl in *.
+     order. auto. auto.
+  - intros y0 e H1; inversion_clear 1; intuition.
 Qed.
 
-Lemma beq_idP : forall x y, reflect (x = y) (beq_id x y).
-Proof. intros x y. apply iff_reflect. now rewrite beq_id_true_iff. Qed.
+End Elt.
 
-Lemma map_extL: forall A (m1 m2: total_map A), (forall x, m1 = m2 -> m1 x = m2 x).
-Proof. intros A m1 m2 x H. now rewrite H. Qed.
+End Raw.
 
-Lemma map_extR: forall A (m1 m2: total_map A), (forall x, m1 x = m2 x -> m1 = m2).
-Proof. intros A m1 m2.
-       About functional_extensionality_dep.
-       About functional_extensionality.
-       intros x H.
-       apply (@functional_extensionality id A m1 m2).
-       destruct x. intro x. destruct x.
-Admitted.
 
