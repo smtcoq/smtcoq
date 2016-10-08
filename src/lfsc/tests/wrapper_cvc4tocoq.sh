34089c719a275542008816accb131fd125e714f3
ekiciburak <ekcburak@hotmail.com>
2016-10-07 20:39:37 -0500

    make sure to give correct path for your benchs...

--- src/lfsc/tests/wrapper_cvc4tocoq.sh
+++ src/lfsc/tests/wrapper_cvc4tocoq.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-OUTPUT_FOLDER=/home/burak/Desktop/smtcoq/src/lfsc/tests/results/
-CVC4TOCOQ_HOME=/home/burak/Desktop/smtcoq/src/lfsc/tests/
+OUTPUT_FOLDER= ... /smtcoq/src/lfsc/tests/results/ #make sure to complete the path
+CVC4TOCOQ_HOME=... /smtcoq/src/lfsc/tests/ #make sure to complete the path
 
 ${CVC4TOCOQ_HOME}/cvc4tocoq $1 &> $1.result
-#exit 0
-# exit'e gerek var mi emin degilim abi
-#${CVC4TOCOQ_HOME}/cvc4tocoq $1 &> ${OUTPUT_FOLDER}$1.result
+
