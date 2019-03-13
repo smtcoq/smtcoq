029b10070aaaa541352e8312d1f2a76689f052e9
Chantal Keller <Chantal.Keller@lri.fr>
2019-03-13 09:10:54 +0100

    Move to _CoqProject

--- src/configure.sh
+++ src/configure.sh
@@ -2,7 +2,7 @@
 
 pre=$(echo $0 | sed "s,\(\([^/]*/\)*\)[^/]*,\1,")
 
-rm -f ${pre}Make
+rm -f ${pre}_CoqProject
 rm -f ${pre}Makefile
 rm -f ${pre}Makefile.conf
 rm -f ${pre}smtcoq_plugin.ml4
@@ -23,7 +23,7 @@ if [ $@ -a $@ = -native ]; then
     cp ${pre}versions/native/smtcoq_plugin_native.ml4 ${pre}smtcoq_plugin.ml4
     cp ${pre}versions/native/Structures_native.v ${pre}versions/native/Structures.v
 else
-    cp ${pre}versions/standard/Make ${pre}Make
+    cp ${pre}versions/standard/_CoqProject ${pre}_CoqProject
     cp ${pre}versions/standard/g_smtcoq_standard.ml4 ${pre}g_smtcoq.ml4
     cp ${pre}versions/standard/smtcoq_plugin_standard.mlpack ${pre}smtcoq_plugin.mlpack
     cp ${pre}versions/standard/Int63/Int63_standard.v ${pre}versions/standard/Int63/Int63.v
@@ -33,6 +33,6 @@ else
     cp ${pre}versions/standard/Int63/Int63Properties_standard.v ${pre}versions/standard/Int63/Int63Properties.v
     cp ${pre}versions/standard/Array/PArray_standard.v ${pre}versions/standard/Array/PArray.v
     cp ${pre}versions/standard/Structures_standard.v ${pre}versions/standard/Structures.v
-    coq_makefile -f Make -o Makefile
-    sed -i Makefile -e "s/ocamldep/ocamldep -native/" -e "s/	\$(HIDE)\$(MAKE) --no-print-directory -f \"\$(SELF)\" post-all//"
+    coq_makefile -f _CoqProject -o CoqMakefile
+    sed -i CoqMakefile -e "s/ocamldep/ocamldep -native/" -e "s/	\$(HIDE)\$(MAKE) --no-print-directory -f \"\$(SELF)\" post-all//"
 fi
