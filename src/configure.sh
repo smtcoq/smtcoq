c27733b9858e983c7dca8d4abf6e355c20e728c8
Chantal Keller <Chantal.Keller@lri.fr>
2019-03-13 11:11:02 +0100

    Switch to Makefile.local instead of -extra

--- src/configure.sh
+++ src/configure.sh
@@ -5,6 +5,7 @@ pre=$(echo $0 | sed "s,\(\([^/]*/\)*\)[^/]*,\1,")
 rm -f ${pre}_CoqProject
 rm -f ${pre}Makefile
 rm -f ${pre}Makefile.conf
+rm -f ${pre}Makefile.local
 rm -f ${pre}smtcoq_plugin.ml4
 rm -f ${pre}versions/native/Structures.v
 rm -f ${pre}g_smtcoq.ml4
@@ -24,6 +25,7 @@ if [ $@ -a $@ = -native ]; then
     cp ${pre}versions/native/Structures_native.v ${pre}versions/native/Structures.v
 else
     cp ${pre}versions/standard/_CoqProject ${pre}_CoqProject
+    cp ${pre}versions/standard/Makefile.local ${pre}Makefile.local
     cp ${pre}versions/standard/g_smtcoq_standard.ml4 ${pre}g_smtcoq.ml4
     cp ${pre}versions/standard/smtcoq_plugin_standard.mlpack ${pre}smtcoq_plugin.mlpack
     cp ${pre}versions/standard/Int63/Int63_standard.v ${pre}versions/standard/Int63/Int63.v
@@ -34,5 +36,5 @@ else
     cp ${pre}versions/standard/Array/PArray_standard.v ${pre}versions/standard/Array/PArray.v
     cp ${pre}versions/standard/Structures_standard.v ${pre}versions/standard/Structures.v
     coq_makefile -f _CoqProject -o Makefile
-    sed -i Makefile -e "s/ocamldep/ocamldep -native/" -e "s/	\$(HIDE)\$(MAKE) --no-print-directory -f \"\$(SELF)\" post-all//"
+    sed -i Makefile -e "s/ocamldep/ocamldep -native/"
 fi
