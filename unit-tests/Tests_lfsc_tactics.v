90d3ce10b41159c0bfe266ce403e4b5f31d970cf
Chantal Keller <Chantal.Keller@lri.fr>
2020-01-16 12:16:12 +0100

    Add the full example of the webpage in the unit tests

--- unit-tests/Tests_lfsc_tactics.v
+++ unit-tests/Tests_lfsc_tactics.v
@@ -698,6 +698,36 @@ Section A_BV_EUF_LIA_PR.
 End A_BV_EUF_LIA_PR.
 
 
+(* Example of the webpage *)
+
+Section group.
+  Variable e : Z.
+  Variable inv : Z -> Z.
+  Variable op : Z -> Z -> Z.
+
+  Hypothesis associative :
+    forall a b c, op a (op b c) =? op (op a b) c.
+  Hypothesis identity : forall a, (op e a =? a).
+  Hypothesis inverse : forall a, (op (inv a) a =? e).
+
+  Add_lemmas associative identity inverse.
+
+  Lemma inverse' :
+    forall a : Z, (op a (inv a) =? e).
+  Proof. smt. Qed.
+
+  Lemma identity' :
+    forall a : Z, (op a e =? a).
+  Proof. smt inverse'. Qed.
+
+  Lemma unique_identity e':
+    (forall z, op e' z =? z) -> e' =? e.
+  Proof. intros pe'; smt pe'. Qed.
+
+  Clear_lemmas.
+End group.
+
+
 (*
    Local Variables:
    coq-load-path: ((rec "../src" "SMTCoq"))
