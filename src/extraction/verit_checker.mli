aa64adfaa879f493dc8df2b64e32e408afccb567
Chantal Keller <Chantal.Keller@lri.fr>
2022-07-29 17:29:44 +0200

    mli file

---
+++ src/extraction/verit_checker.mli
@@ -0,0 +1,13 @@
+(**************************************************************************)
+(*                                                                        *)
+(*     SMTCoq                                                             *)
+(*     Copyright (C) 2011 - 2022                                          *)
+(*                                                                        *)
+(*     See file "AUTHORS" for the list of authors                         *)
+(*                                                                        *)
+(*   This file is distributed under the terms of the CeCILL-C licence     *)
+(*                                                                        *)
+(**************************************************************************)
+
+
+val checker : string -> string -> bool
