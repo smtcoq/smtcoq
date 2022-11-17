2091c4607289de6e2be9cb306d756484e2c61303
Arthur Correnson <arthur.correnson@gmail.com>
2022-11-17 11:23:48 +0100

    [fix] Fix sed invocation in configure.sh
    The sed command (line 4 in src/configure.sh) was used without an explicit argument
    for the option -i. This is undefined for some implementations of sed (for example the BSD one).

--- src/configure.sh
+++ src/configure.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 coq_makefile -f _CoqProject -o Makefile
-sed -i 's/^CAMLDONTLINK=unix,str$/CAMLDONTLINK=num,str,unix,dynlink,threads/' Makefile
+sed -i '' 's/^CAMLDONTLINK=unix,str$/CAMLDONTLINK=num,str,unix,dynlink,threads/' Makefile
