7640a63380f5c2dee7043cc40b429927cf755e07
Arjun Viswanathan <arjun.viswanathan23@gmail.com>
2023-11-15 18:13:21 +0530

    Adding abduce tactic (#129)
    Add to SMTCoq the abduce capabilities of CVC5
    
    ---------
    
    Co-authored-by: Chantal Keller <ckeller@lmf.cnrs.fr>

--- src/Tactics.v
+++ src/Tactics.v
@@ -191,6 +191,19 @@ Tactic Notation "verit"           :=
          [ .. | verit_bool_base_auto Hs; vauto ]
   ])).
 
+  Tactic Notation "abduce" int_or_var(i) :=
+  let tac :=
+  ltac2:(i |- intros ; get_hyps_cont_ltac1
+  (ltac1:(i hs |-
+  add_compdecs hs;
+  [ .. | prop2bool;
+         lazymatch hs with
+         | Some ?hs => prop2bool_hyps hs
+         | None => idtac
+         end;
+         [ .. | cvc5_bool_abduct i hs; vauto ]
+  ]) i)) in tac i.
+
 Tactic Notation "verit_no_check" constr(h) :=
   let tac :=
   ltac2:(h |- intros; get_hyps_cont_ltac1  (ltac1:(h Hs |- let Hs :=
