bde543c758d37cbd7fe63626ee68f3c030f1bc2c
Chantal Keller <ckeller@lmf.cnrs.fr>
2024-06-19 15:06:52 +0200

    An example (that does not go through)

--- src/extraction/testapi.ml
+++ src/extraction/testapi.ml
@@ -86,6 +86,36 @@ let testC03 =
   (smt, proof)
 
 
+(* unit-tests/lia6.vtlog *)
+
+let test_lia6 =
+  let fx = ("x", [], "Int") in
+  let x = Api.EFun (fx, []) in
+  let e4 = Api.EMinus (x, Api.EInt 3) in
+  let e5 = Api.ELe (Api.EInt 7, e4) in
+  let e3 = Api.ELe (e4, EInt 7) in
+  let e2 = Api.EBAnd (e3, e5) in
+  let e6 = Api.EGe (x, Api.EInt 10) in
+  let e1 = Api.EImp (e2, e6) in
+  let smt =
+    let sorts = [] in
+    let funs = [fx] in
+    let f = Api.ENeg e1 in
+    let ass = [|f|] in
+    (sorts, funs, ass)
+  in
+  let proof =
+    let t1 = ("t1", Api.Cassume 0) in
+    let t2 = ("t2", Api.Cnot_implies1 t1) in
+    let t3 = ("t3", Api.Cand (t2, 1)) in
+    let t4 = ("t4", Api.Cnot_implies2 t1) in
+    let t5 = ("t5", Api.Clia_generic [e6; Api.ENeg e5]) in
+    let t6 = ("t6", Api.Cresolution [t5; t3; t4]) in
+    t6
+  in
+  (smt, proof)
+
+
 (* Error when terms are ill-typed *)
 
 let testT00 =
@@ -108,6 +138,7 @@ let _ =
   assert (let (smt, proof) = testC01 in Api.checker smt proof);
   assert (let (smt, proof) = testC02 in Api.checker smt proof);
   assert (let (smt, proof) = testC03 in Api.checker smt proof);
+  assert (let (smt, proof) = test_lia6 in Api.checker smt proof);
   Printf.printf "All tests suceeded\nNow testing the debugging checker:\n";
   Printf.printf "testI00:\n";
   let (smt, proof) = testI00 in Debug_checker.debug_checker_stdout smt proof;
