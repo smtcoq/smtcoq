2ac9292abbd0fe773f3bb4a26bbb1ec5ffcefdf0
Vincent Lafeychine <vincent.lafeychine@proton.me>
2026-06-08 23:33:59 +0200

    feat(lfsc): Tests using generated Dune file

--- unit-tests/runverit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-count=1
-i=$1
-file=$(echo "$i" | sed "s/.smt2/.vtlog/")
-
-veriT --proof-prune --proof-merge --proof-with-sharing --cnf-definitional --disable-ackermann --input=smtlib2 --proof=$file $i
